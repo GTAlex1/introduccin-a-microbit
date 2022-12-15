d9526f1742085ea04e3bbf657c545335f612c252
GTAlex1 <120459752+GTAlex1@users.noreply.github.com>
2022-12-15 12:09:13 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("Alex!")
+basic.pause(500)
+while (true) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+}
+basic.forever(function () {
+	
+})
